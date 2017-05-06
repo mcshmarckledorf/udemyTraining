a485bde9559bf853076dc44bb9d3f91cb73cdff9
mcshmarckledorf <joeygame@gmail.com>
2017-05-05 20:31:41 -0700

    modified:   main.go

--- stringTest/main.go
+++ stringTest/main.go
@@ -1,9 +1,10 @@
 //Testing my stringUtil folder and pulling packages
 package main
 
-import "fmt"
-import "github.com/mcshmarckledorf/stringUtil"
+//imoprt "fmt"
+//import "github.com/mcshmarckledorf..."
+//The test was a success, but I do not want my info to be out there lol
 
-func main() {
-	fmt.Println(stringUtil.MyName + stringUtil.MyEducation + stringUtil.MyAddress)
-}
+//func main() {
+//	fmt.Println(stringUtil.MyName + stringUtil.MyEducation + stringUtil.MyAddress)
+//}
