b5fb6c44c253d8b19773bfa9feb72d840482d628
mcshmarckledorf <joeygame@gmail.com>
2017-05-05 20:10:51 -0700

    new file:   main.go

---
+++ stringTest/main.go
@@ -0,0 +1,9 @@
+//Testing my stringUtil folder and pulling packages
+package main
+
+import "fmt"
+import "github.com/mcshmarckledorf/stringUtil"
+
+func main() {
+	fmt.Println(stringUtil.MyName + stringUtil.MyEducation + stringUtil.MyAddress)
+}
